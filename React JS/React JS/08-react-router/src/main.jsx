44055fe1ac4e5db7a1b66b85cad11026df768635
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-10-25 15:45:50 +0530

    update main

--- React JS/React JS/08-react-router/src/main.jsx
+++ React JS/React JS/08-react-router/src/main.jsx
@@ -1,30 +1,37 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import Home from "./components/Home/Home.jsx";
-import About from "./components/About/About.jsx";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import Root from "./Root";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
-const router = [
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-];
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       { path: "", element: <Home /> },
+//       { path: "about", element: <About /> },
+//       { path: "contact", element: <Contact /> },
+//     ],
+//   },
+//   {},
+// ]);
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="" element={<Home />}></Route>
+      <Route path="about" element={<About />}></Route>
+      <Route path="contact" element={<Contact />}></Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </React.StrictMode>
 );
