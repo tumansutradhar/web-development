978ce7bfcdcd0e3b0eaaa93831928282baf3ba88
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-10-28 18:11:30 +0530

    update main

--- React JS/React JS/08-react-router/src/main.jsx
+++ React JS/React JS/08-react-router/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from "react-router-dom";
 import Root from "./Root";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Github, { githubInfoLoader } from "./components/Github/Github";
 
 // const router = createBrowserRouter([
 //   {
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />}></Route>
       <Route path="about" element={<About />}></Route>
       <Route path="contact" element={<Contact />}></Route>
+      <Route loader={githubInfoLoader} path="github" element={<Github />}></Route>
     </Route>
   )
 );
