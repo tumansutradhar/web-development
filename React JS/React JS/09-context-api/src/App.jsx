5041d9e12faaadadaa535ffb9e9669013fee78fe
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-10-29 19:36:20 +0530

    add context api

---
+++ React JS/React JS/09-context-api/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UserContextProvider from "./context/UserContextProvider"
+
+function App() {
+  return (
+    <UserContextProvider>
+    </UserContextProvider>
+  )
+}
+
+export default App
