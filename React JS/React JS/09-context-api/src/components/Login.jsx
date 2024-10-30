b7cd7d88dcee1c66045bb28e99db5055f9f00f76
tumansutradhar <officialbishalsutradhar@gmail.com>
2024-10-30 22:16:20 +0530

    add new files

---
+++ React JS/React JS/09-context-api/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useContext} from 'react'
+import UserContext from "../context/UserContext";
+
+function Login() {
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  )
+}
+
+export default Login
---
+++ React JS/React JS/09-context-api/src/components/Profile.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useContext } from 'react'
+import UserContext from "../context/UserContext"
+
+export default function Profile() {
+    return (
+        <div>Profile</div>
+    )
+}
